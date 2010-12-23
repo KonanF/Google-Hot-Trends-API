8c984bb40a3474392bd0429fd02e4f07035908e0
ekkans <git@ekkans.com>
2010-12-23 20:12:39 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trends::Application.config.secret_token = 'fb448e810b195377840746768dfea8f0b860f072a9388f4fcdf7f2f13de72b0a90569e295c68987965867c58fbeace3e613eabaa32bbfa182dadc9a63372c695'
---
+++ rb_scripts/trends.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'hpricot'
+require 'rest-open-uri'
+require 'builder'
+
+hdrs = {"User-Agent"=>"Mozilla/5.0 (Macintosh; U; PPC Mac OS X Mach-O; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1", "Accept-Charset"=>"utf-8", "Accept"=>"text/html"}
+my_html = ""
+open("http://www.google.com/trends/hottrends", hdrs).each {|s| my_html << s}
+@web_doc= Hpricot(my_html)
+
+@t = []
+
+(@web_doc/"td.hotColumn").search("a").each do |e|
+  @t.push(e.inner_html)
+end
+
+puts @t
