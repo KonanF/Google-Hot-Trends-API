502e98eeac675fc0ad887ebcab07813f4cfc5ec8
ekkans <git@ekkans.com>
2010-12-23 23:28:07 +0100

    Code cleaning

--- app/controllers/hottrends_controller.rb
+++ app/controllers/hottrends_controller.rb
@@ -6,9 +6,6 @@ class HottrendsController < ApplicationController
   # GET api/hottrends/2009-8-16
   # GET api/hottrends/2009-8-16.xml
   def api
-    @hottrends = Hottrend.all
-    #@hottrend = Hottrend.find(:date => params[:date])
-
     param_date = params[:date].to_date
     if Hottrend.where(:date => param_date).empty?
       create_hot_trends_in_db(params[:date])
@@ -16,36 +13,14 @@ class HottrendsController < ApplicationController
       destroy_hot_trends_in_db(params[:date])
       create_hot_trends_in_db(params[:date])
     end
-
-    # Get Hottrends
+    
     @hottrends = Hottrend.where(:date => param_date)
 
     respond_to do |format|
       format.html # api.html.erb
-      format.xml  { render :xml => @hottrend }
+      format.xml  { render :xml => @hottrends }
     end
   end
-
-  def destroy_hot_trends_in_db(date)
-      hts = Hottrend.where(:date => date.to_date)
-      hts.each do |ht|
-        ht.destroy
-      end
-    end
-
-    def create_hot_trends_in_db(date)
-      hdrs = {"User-Agent"=>"Mozilla/5.0 (Macintosh; U; PPC Mac OS X Mach-O; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1", "Accept-Charset"=>"utf-8", "Accept"=>"text/html"}
-      my_html = ""
-      url = "http://www.google.com/trends/hottrends?date="+date
-      page = open(url, hdrs).each {|s| my_html << s}
-      @web_doc =  Hpricot(my_html)
-
-      @i = 0
-      (@web_doc/"td.hotColumn").search("a").each do |e|
-        Hottrend.create(:date => date.to_date, :text => e.inner_html, :num => @i, :culture => "en_US")
-        @i += 1
-      end
-    end
 
   # GET /hottrends
   # GET /hottrends.xml
@@ -128,4 +103,26 @@ class HottrendsController < ApplicationController
       format.xml  { head :ok }
     end
   end
+
+  def destroy_hot_trends_in_db(date)
+      hts = Hottrend.where(:date => date.to_date)
+      hts.each do |ht|
+        ht.destroy
+      end
+    end
+
+  def create_hot_trends_in_db(date)
+    hdrs = {"User-Agent"=>"Mozilla/5.0 (Macintosh; U; PPC Mac OS X Mach-O; en-US; rv:1.8.1.1) Gecko/20061204 Firefox/2.0.0.1", "Accept-Charset"=>"utf-8", "Accept"=>"text/html"}
+    my_html = ""
+    url = "http://www.google.com/trends/hottrends?date="+date
+    page = open(url, hdrs).each {|s| my_html << s}
+    @web_doc =  Hpricot(my_html)
+
+    @i = 0
+    (@web_doc/"td.hotColumn").search("a").each do |e|
+      Hottrend.create(:date => date.to_date, :text => e.inner_html, :num => @i, :culture => "en_US")
+      @i += 1
+    end
+  end
+
 end
